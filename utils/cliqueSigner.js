0ef8d752786ceb77cd371915f9f84eac9ae35bc0
adriamb <adria@codecontext.io>
2018-10-11 18:48:24 +0000

    fixed signer

--- utils/cliqueSigner.js
+++ utils/cliqueSigner.js
@@ -2,6 +2,7 @@ var ethUtil = require('ethereumjs-util');
 var ethBlock = require('ethereumjs-block');
 
 function cliqueSigner (blockParams) {
+
       const blockHeader = new ethBlock.Header({
         parentHash: blockParams.parentHash,
         uncleHash: blockParams.sha3Uncles,
@@ -10,7 +11,7 @@ function cliqueSigner (blockParams) {
         transactionsTrie: blockParams.transactionsRoot,
         receiptTrie: blockParams.receiptsRoot,
         bloom: blockParams.logsBloom,
-        difficulty: blockParams.difficulty,
+        difficulty: parseInt(blockParams.difficulty),
         number: blockParams.number,
         gasLimit: blockParams.gasLimit,
         gasUsed: blockParams.gasUsed,
@@ -19,10 +20,11 @@ function cliqueSigner (blockParams) {
         mixHash: blockParams.mixHash,
         nonce: blockParams.nonce
       })
-  
+ 
       msgHash = ethUtil.sha3(ethUtil.rlp.encode(blockHeader.raw))
       signature = ethUtil.fromRpcSig('0x'+blockParams.extraData.substring(2+32*2))
       publicKey = ethUtil.ecrecover(msgHash,signature.v,signature.r,signature.s)
+
       return '0x'+ethUtil.pubToAddress(publicKey).toString('hex')
 }
 
